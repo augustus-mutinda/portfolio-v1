7424d3fe1f893104493df609aedf44e638de4ec9
augustus_iam <augustusmutinda16@gmail.com>
2024-07-25 09:33:13 +0300

    Added Sign Off

--- components/SignOff.vue
+++ components/SignOff.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="text-sm text-slate-500 px-4 font-inter font-normal pb-16">
+    <p class="text-sm text-slate-500 px-4 font-inter font-normal pb-2">
       Loosely designed in <a class="text-slate-400 hover:text-emerald-500" href="https://penpot.app/" target="_blank">Penpot</a>
       and coded in
       <a class="text-slate-400 hover:text-emerald-500" href="https://www.jetbrains.com/webstorm/" target="_blank">Webstorm</a>
@@ -13,7 +13,10 @@
       . All text is set in the Inter
       typeface.
     </p>
+
+    <p class="text-sm text-slate-500 px-4 font-inter font-normal pb-16">All rights reserved. 2023 - {{ year }} Augustus Mutinda.</p>
   </div>
 </template>
 <script setup lang="ts">
+const year = new Date().getFullYear();
 </script>
